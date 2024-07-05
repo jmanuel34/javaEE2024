4d9268027fe7be20ec0cd686c359dce94e6f93a6
Jose Manuel Mendez <jmanuel34@gmail.com>
2024-07-05 09:05:32 +0200

    cursos

---
+++ 45_formacion_ajax_jm/src/main/java/service/CursosService.java
@@ -0,0 +1,38 @@
+package service;
+
+import java.util.List;
+
+import dao.CursosDao;
+import model.Curso;
+
+public class CursosService{
+	CursosDao dao=new CursosDao();
+
+	public boolean nuevo(Curso curso) {		
+		if(dao.findByNombre(curso.getNombre())==null) { //si es null, el curso no existe y se añade
+			dao.save(curso);
+			return true;
+		}
+		return false;	
+	}
+	
+	public List<Curso> preciosCursoMax(double precioMax){
+		return dao.findByPrecioMax(precioMax);
+	}
+	
+	public boolean eliminarCurso(String nombre) {
+		if(dao.findByNombre(nombre)!=null) { //si es null, el curso no existe
+			dao.deleteByNombre(nombre);
+			return true;
+		}
+		else return false;
+	}
+	
+	public boolean modificarDuracion(String nombre, int nuevaDuracion) {
+		if(dao.findByNombre(nombre)!=null) { //si es null, el curso no existe y se añade
+			dao.updateDuracion(nombre, nuevaDuracion);
+			return true;
+		}
+		return false;
+	}
+}
